ef684b973501d6c5a7e89ecff213306a5c874d7e
Tejaswini Boddanam <bdeepubdeepu370@gmail.com>
2026-07-22 22:03:29 +0530

    Add files via upload

---
+++ Day 2/Keyword_program.py
@@ -0,0 +1,7 @@
+Python 3.14.6 (tags/v3.14.6:c63aec6, Jun 10 2026, 10:26:10) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> import keyword
+>>> print(keyword.kwlist)
+['False', 'None', 'True', 'and', 'as', 'assert', 'async', 'await', 'break', 'class', 'continue', 'def', 'del', 'elif', 'else', 'except', 'finally', 'for', 'from', 'global', 'if', 'import', 'in', 'is', 'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'try', 'while', 'with', 'yield']
+>>> print(len(keyword.kwlist))
+35
---
+++ Day 2/variable_operations.py
@@ -0,0 +1,27 @@
+Python 3.14.6 (tags/v3.14.6:c63aec6, Jun 10 2026, 10:26:10) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> a=10
+>>> a
+10
+>>> a,b,c=10,20,30
+>>> c
+30
+>>> a=b=c=10
+>>> b
+10
+>>> a,b=b,a
+>>> b
+10
+>>> s=1
+>>> r=1
+>>> s+r
+2
+>>> del r
+>>> r
+Traceback (most recent call last):
+  File "<pyshell#12>", line 1, in <module>
+    r
+NameError: name 'r' is not defined
+>>> myvar=30
+>>> myvar
+30
