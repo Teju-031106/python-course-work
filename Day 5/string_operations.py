456f208dba934f9ebc699516007e6386adab2737
Tejaswini Boddanam <bdeepubdeepu370@gmail.com>
2026-07-24 23:12:44 +0530

    Add files via upload

---
+++ Day 5/string_operations.py
@@ -0,0 +1,55 @@
+Python 3.14.6 (tags/v3.14.6:c63aec6, Jun 10 2026, 10:26:10) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+s=''
+s
+''
+s='codegnan'
+s
+'codegnan'
+'codegnan'+'pfs'
+... 'codegnanpfs'
+... 'codegnan'*10
+... 'codegnancodegnancodegnancodegnancodegnancodegnancodegnancodegnancodegnancodegnan'
+... '*' * 10
+... '**********'
+... '-' *5
+... '-----'
+... >>> s1='reena'
+... >>> s1[0]
+... 'r'
+... >>> s1[-1]
+... 'a'
+... >>> s1[-3]
+... 'e'
+... >>> s1[3]
+... 'n'
+... >>> s1='reena','kowsar','nandu'
+... >>> s1
+... ('reena', 'kowsar', 'nandu')
+... >>> s1[0]
+... 'reena'
+... >>> s1[-1]
+... 'nandu'
+... >>> s1='reena kowsar nandu'
+... >>> s1
+... 'reena kowsar nandu'
+... >>> s1[0]
+... 'r'
+... >>> s1[:5]
+... 'reena'
+... >>> s1[-1:-6:-1]
+... 'udnan'
+... >>> s1[12:]
+... ' nandu'
+... >>> s1[::-1]
+... 'udnan raswok aneer'
+... >>> s1[-12::-1]
+... 'k aneer'
+... >>> s1[-13:-1]
+... ' kowsar nand'
+... >>> s1[-13::-1]
+... ' aneer'
+... >>> 'reena' in s1
+... True
+... >>> 'star' in s1
+... False
